60c44edbe6a77e714358c69428c392c3b634c258
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-11-24 15:59:48 +0100

    feat(host): set app as host application

--- host/src/main.ts
+++ host/src/main.ts
@@ -1,6 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { initFederation } from '@angular-architects/native-federation';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+initFederation({
+  'mfe1': 'http://localhost:3000/remoteEntry.json'
+})
+  .catch(err => console.error(err))
+  .then(_ => import('./bootstrap'))
+  .catch(err => console.error(err));
