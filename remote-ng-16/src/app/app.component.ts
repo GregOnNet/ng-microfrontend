e92afa5516b4ccbb65d8e37f97bcb186cf0a3b46
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-11-27 11:13:01 +0100

    feat(ng-16): connect router

--- remote-ng-16/src/app/app.component.ts
+++ remote-ng-16/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { connectRouter } from './connect-router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'remote-ng-16';
+
+  constructor() {
+    connectRouter();
+  }
 }
