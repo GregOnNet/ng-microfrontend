f5c77b979042349485a61d06028165f7fbe2a0d1
Gregor Woiwode <gregor.woiwode@gmail.com>
2023-12-05 22:01:24 +0100

    lab: break navigation intentionally by using child routes

--- remote-ng-17/src/app/routes.ts
+++ remote-ng-17/src/app/routes.ts
@@ -4,21 +4,14 @@ import { PostComponent } from './posts/post.component';
 import { PostsComponent } from './posts/posts.component';
 
 export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'posts' },
   {
     path: 'posts',
-    pathMatch: 'full',
-    redirectTo: 'posts/list',
-  },
-  {
-    path: 'posts/list',
     component: PostsComponent,
+    children: [{ path: ':postId', component: PostComponent }],
   },
   {
     path: 'posts/german-now',
     component: GermanNowComponent,
-  },
-  {
-    path: 'posts/:postId',
-    component: PostComponent,
   },
 ];
