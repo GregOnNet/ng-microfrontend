8fa8483d4bd03ef5e7b93148a728948bfa9d6662
Gregor Woiwode <gregor.woiwode@gmail.com>
2023-12-08 21:29:25 +0100

    feat(navigation): get deep links to work :rocket:

--- remote-ng-17/src/app/posts/posts.component.ts
+++ remote-ng-17/src/app/posts/posts.component.ts
@@ -1,26 +1,31 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [AsyncPipe, RouterLink],
+  imports: [AsyncPipe, RouterLink, RouterOutlet],
   template: `
-    <div class="text-orange-500">
-      <h2 class="text-xl font-medium mb-4">Post Titles</h2>
-      <ul class="list-disc list-inside">
-        @for(post of posts$ | async; track post.id) {
-        <li>
-          <a [routerLink]="['/posts', post.id]" class="hover:underline">{{
-            post.title
-          }}</a>
-        </li>
-        } @empty {
-        <li>No posts found.</li>
-        }
-      </ul>
+    <h2 class="text-xl font-medium mb-4">Post Titles</h2>
+    <div class="grid-cols-2">
+      <nav>
+        <ul class="list-disc list-inside text-blue-600">
+          @for(post of posts$ | async; track post.id) {
+          <li>
+            <a [routerLink]="['/posts', post.id]" class="hover:underline">{{
+              post.title
+            }}</a>
+          </li>
+          } @empty {
+          <li>No posts found.</li>
+          }
+        </ul>
+      </nav>
+      <aside>
+        <router-outlet></router-outlet>
+      </aside>
     </div>
   `,
 })
